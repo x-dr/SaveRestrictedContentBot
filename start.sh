913b1fd11ef95874a086fcec0f2d8c910ffb7602
x-dr <xd9722@gmail.com>
2022-07-25 00:37:56 +0800

    first commit

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "starting Bot";
-python3 -m main
-
+echo "starting nginx"
 
 /bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
+
+echo "starting Bot";
+python3 -m main
